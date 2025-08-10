a86325f834b811b161f0c8175e2fa8384656510b
Ebrahimim29 <Ebrahimim29@gmail.com>
2025-08-10 06:08:31 +0330

    part126

--- assets/Java.js
+++ assets/Java.js
@@ -1,13 +1,39 @@
+const orderList = document.getElementById("ordersUl");
+const drinkText = document.getElementById("drinkText");
+const errorBox = document.getElementById("errorMsg");
+const customerName = document.getElementById("nameInput");
+const drink = document.getElementById("drinkSelect");
+
 function submitOrder() {
-    const customerName = document.getElementById("nameInput").value;
-    const drink = document.getElementById("drinkSelect").value;
-    const orderList = document.getElementById("ordersUl");
+    const customerNameValue = customerName.value.trim();
+    const drinkValue = drink.value;
 
-    // orderList.innerText = `😍 ${customerName} - 🥤 ${drink}`;
-
+    // orderList.innerText = `😍 ${customerNameValue} - 🥤 ${drinkValue}`;
+    
+    if (customerNameValue === "" || !drinkValue) {
+        errorBox.innerText = "لطفا نام و نوشیدنی را وارد کنید! 🙄";
+    } else {
+        drinkText.innerText = `انتخاب شما : ${drinkValue}`;
+    }
+    
+    // ایجاد یک آیتم لیست جدید
     const newOrder = document.createElement("li");
-    newOrder.innerText = `😍 ${customerName} - 🥤 ${drink}`;
-
+    newOrder.innerText = `😍 ${customerNameValue} - 🥤 ${drinkValue}`;
     //appendChild():برای اضافه کردن انواع المان کامنت و محتوای متنی به عنوان فرزند
     orderList.appendChild(newOrder);
+
+    //پاک کردن ورودی ها
+    customerName.value = "";
+    drink.value = "";
+}
+
+function toggleTheme() {
+    const card = document.getElementById("drinkCard");
+    card.classList.toggle("dark");
+}
+
+//innerHtml:پراپرتی شبیه به innerText که توسط آن المانهای اچ تی ام ال داخل تگ را تغییر یا نمایش داد
+
+function reset() {
+    orderList.innerHTML = "";
 }
