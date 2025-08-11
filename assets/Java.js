159d9c48626dd6b5feab48bbb91698c268710f53
Ebrahimim29 <Ebrahimim29@gmail.com>
2025-08-11 06:02:17 +0330

    part128

--- assets/Java.js
+++ assets/Java.js
@@ -13,14 +13,20 @@ function submitOrder() {
     if (customerNameValue === "" || !drinkValue) {
         errorBox.innerText = "لطفا نام و نوشیدنی را وارد کنید! 🙄";
     } else {
+        errorBox.textContent = "";
         drinkText.innerText = `انتخاب شما : ${drinkValue}`;
     }
     
     // ایجاد یک آیتم لیست جدید
-    const newOrder = document.createElement("li");
-    newOrder.innerText = `😍 ${customerNameValue} - 🥤 ${drinkValue}`;
+    // const newOrder = document.createElement("li");
+    // newOrder.innerText = `😍 ${customerNameValue} - 🥤 ${drinkValue}`;
     //appendChild():برای اضافه کردن انواع المان کامنت و محتوای متنی به عنوان فرزند
-    orderList.appendChild(newOrder);
+    // orderList.appendChild(newOrder);
+
+    //append():اضافه به انتها
+    //prepend():اضافه به ابتدا
+
+    orderList.innerHTML = `<li> 😍 ${customerNameValue} - 🥤 ${drinkValue}</li>`
 
     //پاک کردن ورودی ها
     customerName.value = "";
@@ -34,6 +40,10 @@ function toggleTheme() {
 
 //innerHtml:پراپرتی شبیه به innerText که توسط آن المانهای اچ تی ام ال داخل تگ را تغییر یا نمایش داد
 
-function reset() {
-    orderList.innerHTML = "";
-}
+// function reset() {
+    // orderList.innerHTML = "";
+    // orderList.replaceChildren();
+    //متدی برای جایگزین کردن المان و محتوا به جای المان های قبلی و امنیت و سرعت بالاتری نسبت به اینر اچ تی ام ال 
+// }
+
+const reset = ()=> orderList.replaceChildren();
